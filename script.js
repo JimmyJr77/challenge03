059bc8a6535490a14107ce4a2e73092a3c425424
Jimmy O'Brien <jimmy_mac@Jimmys-MBP.askey.com>
2023-05-30 23:08:20 -0400

    Added the framework for building the password generator.

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+//Determine the requirements for the password
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+//Create the pool of characters from which the password will be generated
 
-  passwordText.value = password;
+//Generate the random password
 
-}
+  //Selection: Write a function to select a random character from the character pool (use Math.random()?)
+  //Repeat Selection: Repeat this process until the length of the password required is reached. (use a loop for this?)
+  //Concatenation: Select each random character & add it to the password string
+  //Display the password (use the DOM to display the password?)
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+
+//   // Assignment Code
+// var generateBtn = document.querySelector("#generate");
+
+// // Write password to the #password input
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
+
+//   passwordText.value = password;
+
+// }
+
+// // Add event listener to generate button
+// generateBtn.addEventListener("click", writePassword);
+
